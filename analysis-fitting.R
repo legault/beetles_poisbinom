5beb890b3933580b1b40107e30d5d672f1a5879e
Geoffrey Legault <thegeoffrey@gmail.com>
2019-08-05 15:47:42 -0400

    update to analysis script
    script now sources the rounded counts for cs and cf

--- analysis-fitting.R
+++ analysis-fitting.R
@@ -19,54 +19,9 @@ set.seed(20190422)
 # Source negative log-likelihood function (general)
 source("nll.R")$value
 
-# Create rounding function
-mround <- function(x, base){
-        base * round(x / base)
-}
-
-# Source data
-cs <- read.csv("data/csDat.csv", header = TRUE)
-# Round counts
-cs$numCS1b.r <- mround(cs$numCS1b, 10)
-cs$numCS2b.r <- mround(cs$numCS2b, 10)
-cs$numCF1b.r <- mround(cs$numCF1b, 10)
-cs$numCF2b.r <- mround(cs$numCF2b, 10)
-cs$numCS1a.r <- mround(cs$numCS1a, 10)
-cs$numCS2a.r <- mround(cs$numCS2a, 10)
-cs$numCF1a.r <- mround(cs$numCF1a, 10)
-cs$numCF2a.r <- mround(cs$numCF2a, 10)
-# Sum patches
-cs$totCSb.r <- cs$numCS1b.r + cs$numCS2b.r
-cs$totCSa.r <- cs$numCS1a.r + cs$numCS2a.r
-cs$totCFb.r <- cs$numCF1b.r + cs$numCF2b.r
-cs$totCFa.r <- cs$numCF1a.r + cs$numCF2a.r
-## Totals
-cs$totCS.r <- cs$totCSa.r + cs$totCSb.r
-cs$totCF.r <- cs$totCFa.r + cs$totCFb.r
-## Diff
-cs$totCSdiff.r <- cs$totCSa.r - cs$totCSb.r
-cs$totCFdiff.r <- cs$totCFa.r - cs$totCFb.r
-
-# Extract relevant information from data
-## CS
-## Subset
-cs.r <- subset(cs, totCSdiff.r == 0 & totCFdiff.r == 0 & totCS.r > 0)
-## Create smaller table of relevant data
-csdata <- data.frame(patch1.before = cs.r[, "numCS1b.r"],
-                     patch2.before = cs.r[, "numCS2b.r"],
-                     patch1.after = cs.r[, "numCS1a.r"],
-                     patch2.after = cs.r[, "numCS2a.r"],
-                     patch1.other.before = cs.r[, "numCF1b.r"],
-                     patch2.other.before = cs.r[, "numCF2b.r"])
-## CF
-cf.r <- subset(cs, totCSdiff.r == 0 & totCFdiff.r == 0 & totCF.r > 0)
-## Create smaller table of relevant data
-cfdata <- data.frame(patch1.before = cf.r[, "numCF1b.r"],
-                     patch2.before = cf.r[, "numCF2b.r"],
-                     patch1.after = cf.r[, "numCF1a.r"],
-                     patch2.after = cf.r[, "numCF2a.r"],
-                     patch1.other.before = cf.r[, "numCS1b.r"],
-                     patch2.other.before = cf.r[, "numCS2b.r"])
+# Source (rounded) data
+csdata <- read.csv("data/csdata.csv", header = TRUE)
+cfdata <- read.csv("data/cfdata.csv", header = TRUE)
 
 # Create empty table of estimates
 estimates <- data.frame(Species = c(),
@@ -83,11 +38,7 @@ estimates <- data.frame(Species = c(),
 
 # CS
 ## Constant dispersal rate
-### Estimate baseline dispersal rate (p0)
-cs.gen1 <- subset(cs.r, gen == 1 & numCS1b.r > 0) # generation 1
-propd <- mean(cs.gen1$numCS2a.r) / mean(cs.gen1$numCS1b.r) # proportion dispersed
-### Set estimate propd as starting value for p0
-startingvalues <- list(p0 = log(propd / (1 - propd)))
+startingvalues <- list(p0 = -1)
 ### Run optimizer
 for(i in 1){
     arg.temp <- formalArgs(paste(m.list[i]))
@@ -158,14 +109,9 @@ for(i in 2:length(m.list)){
     }
 }
 
-
 # CF
 ## Constant dispersal rate
-### Estimate baseline dispersal rate (p0)
-cf.gen1 <- subset(cf.r, gen == 1 & numCF2b.r > 0) # generation 1
-propd <- mean(cf.gen1$numCF1a.r) / mean(cf.gen1$numCF2b.r) # proportion dispersed
-### Set estimate propd as starting value for p0
-startingvalues <- list(p0 = log(propd / (1 - propd)))
+startingvalues <- list(p0 = -1)
 ### Run optimizer
 for(i in 1){
     arg.temp <- formalArgs(paste(m.list[i]))
@@ -235,7 +181,6 @@ for(i in 2:length(m.list)){
         startingvalues.temp <- as.list(fit$par)
     }
 }
-
 
 # Summary
 estimates.summary <- subset(estimates, Model == c("model0"))
